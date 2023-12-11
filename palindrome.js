e453cb85e05abc414e2cfa3e7685efc2943a28c7
Sarthak Rathore <sarthakrathore38@gmail.com>
2023-12-11 15:10:53 +0530

    palindrome file

---
+++ palindrome.js
@@ -0,0 +1,37 @@
+// function isPalindrome(str) {
+//     let i = 0;
+//     let j = str.length - 1;
+  
+//     while (i < j) {
+//       if (str[i] !== str[j]) {
+//         return false;
+//       }
+  
+//       i++;
+//       j--;
+//     }
+  
+//     return true;
+//   }
+  
+//   const str = "nayans";
+//   console.log(isPalindrome(str)); // Output: true
+
+
+
+// function isPalindrome(str) {
+//     const reversedStr = str.split("").reverse().join("");
+//     return str === reversedStr;
+//   }
+  
+//   console.log(isPalindrome("racecar")); // Output: true
+
+
+
+  
+
+
+
+
+  
+  

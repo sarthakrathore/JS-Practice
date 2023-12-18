83c318ff22527cb6fd5ec2c3bb9fb3453eda2805
Sarthak Rathore <sarthakrathore38@gmail.com>
2023-12-18 18:55:15 +0530

    18/12/2023

--- FilterMapReduce.js
+++ FilterMapReduce.js
@@ -24,14 +24,70 @@
 
 // used because foreach doesnt 'return' 
 
-const myNum=[1,2,3,4,5,6,7,8,9,10]
-const newNum=myNum.map((num)=>{
+// const myNum=[1,2,3,4,5,6,7,8,9,10]
+// const newNum=myNum.map((num)=>{
 
-   return num+10
+//    return num+10
    
-})
+// })
 
-console.log(newNum);
+// console.log(newNum);
+
+
+// Reduce method
+// can be used in shopping carts to add item prices
+
+// const myNums=[1,2,3]
+
+// const myTotal=myNums.reduce((acc,currval)=>{
+
+//    console.log(`acc "${acc} and currval: ${currval}`);
+
+//    return acc+currval
+
+// },0 ) //initial value
+// console.log(myTotal);
+
+
+
+const shoppingCart=[
+   {
+      itemname:"JS basic course",
+      price:599
+   },
+   {
+      itemname:"c++ course",
+      price:1599
+   },
+   {
+      itemname:"Mobile Dev course",
+      price:2599
+   },
+   {
+      itemname:"Data Science",
+      price:12599
+   },
+]
+
+
+const add=shoppingCart.reduce((acc,item)=>{
+
+   if(typeof (item.price===Number)){
+
+      return  acc+ item.price
+
+   }
+
+   else{
+
+      console.log("ERROR");
+   }
+
+  
+
+},0)
+
+console.log(add);
 
 
 

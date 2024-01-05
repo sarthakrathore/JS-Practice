4eeb5151c52e377a46a60db0592b2b23586548f4
Sarthak Rathore <sarthakrathore38@gmail.com>
2024-01-05 19:02:37 +0530

    5/1/2024

--- Project5/index.js
+++ Project5/index.js
@@ -1,13 +1,16 @@
+let color
 const randomColor=function(){
+
     const hex="0123456789ABCDEF"
-    let color='#'
+    color='#'
     for (let i = 0; i <6; i++) {
 
-        color+=hex[Math.floor(Math.random()*16)]
+        color+=hex[Math.floor(Math.random()*10)]
             
     }
-
+    console.log(typeof color);
     return color
+    
 }
 
 let intervalId
@@ -18,13 +21,26 @@ const startChangingColor= function(){
     intervalId=setInterval(() => {
 
         document.body.style.backgroundColor=randomColor() 
-    }, 1000);
+
+    }, 100);
 
 }
 
 const stopChangingColor=function(){
 
     clearInterval(intervalId)
+    if (color=="red" || color=="blue" ||color=="green" ||color=="yellow" ||color=="lime" ||color=="purple") {
+
+        document.querySelector('#result').innerHTML=" Sorry! You Lost."
+        
+    }
+
+    else{
+
+        document.querySelector('#result').innerHTML="Yay . You Won!!!"
+        document.querySelector('#para').innerHTML="Click on start to try again"
+
+    }
     console.log("Stopped");
 }
 
